d3ef3518f8fdb3a2c4a8f5e56068eef022ebfb0b
George Kamtziridis <georgekam96@gmail.com>
2018-01-21 22:02:56 +0200

    Initial commit

---
+++ mean_shift_global_seeds.cu
@@ -0,0 +1,203 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <sys/time.h>
+#include <cuda.h>
+
+#define SIZE 210
+#define DIMENSIONS 7
+
+__global__ void mean_shift(double *y, double *x, double *h, double *kernel, double *i_vectors, double *norm)
+{
+  //Thread scoped variables.
+  int basic_index = blockIdx.x * (DIMENSIONS);
+  int index = threadIdx.x * (DIMENSIONS);
+  double e = 0.0001;
+  double factor;
+
+  //Initializing the norm.
+  norm[blockIdx.x] = 1.0;
+
+  //Main loop.
+  while (norm[blockIdx.x] > e) {
+
+    factor = 0.0;
+    //Calculating factor.
+    for (int i = 0; i < (DIMENSIONS); i++)
+    {
+        factor += (y[basic_index + i] - x[index + i])*(y[basic_index + i] - x[index + i]);
+    }
+
+    if (sqrt(factor) > *h*(*h))
+    {
+      kernel[blockIdx.x * SIZE + threadIdx.x] = 0.0;
+      for (int i = 0; i < (DIMENSIONS); i++)
+      {
+        i_vectors[blockIdx.x * SIZE * DIMENSIONS + index+i] = 0.0;
+      }
+    }
+    else
+    {
+      factor = exp(-(factor)/(2* (*h) * (*h)));
+
+      kernel[blockIdx.x * SIZE + threadIdx.x] = factor;
+
+      //Storing the 'y' vectors before normalization.
+      for (int i = 0; i < (DIMENSIONS); i++)
+      {
+        i_vectors[blockIdx.x * SIZE * DIMENSIONS + index+i] = factor * x[index + i];
+      }
+    }
+
+    __syncthreads();
+
+    //Only the first thread of each block will calculate
+    //the final vectors.
+    if (threadIdx.x == 0)
+    {
+      double y_new[DIMENSIONS];
+      for (int i = 0; i < (DIMENSIONS); i++)
+      {
+	       y_new[i] = 0.0;
+      }
+
+      double sum = 0.0;
+
+      for (int i = 0; i < (SIZE) * (DIMENSIONS); i += (DIMENSIONS))
+      {
+      	for (int j = 0; j < (DIMENSIONS); j++)
+      	{
+      	  y_new[j] += i_vectors[blockIdx.x * SIZE * DIMENSIONS + i + j];
+      	}
+      }
+
+      //Calculating the sum of each row of the kernel_matrix.
+      for (int i = 0; i < (SIZE); i++)
+      {
+        sum += kernel[blockIdx.x * SIZE + i];
+      }
+
+      //Normalizing and updating the 'y' vector.
+      for (int i = 0; i < (DIMENSIONS); i++)
+      {
+        y_new[i] = y_new[i] / sum;
+      }
+
+
+      //Re-initializing norm.
+      norm[blockIdx.x] = 0.0;
+
+      //Calculating the norm of the vector
+      //and updating the 'y' vector
+      for (int i = 0; i < (DIMENSIONS); i++)
+      {
+
+        norm[blockIdx.x] += (y_new[i]-y[basic_index + i])*(y_new[i]-y[basic_index + i]);
+        y[basic_index + i] = y_new[i];
+
+      }
+
+      norm[blockIdx.x] = sqrt(norm[blockIdx.x]);
+
+    }
+
+    __syncthreads();
+
+  }
+
+}
+
+int main(int argc, char **argv)
+{
+    FILE *file;
+    double *y, *x, *z, h, *kernel, *i_vectors, *norm;
+    double *d_y, *d_x, *d_h, *d_kernel, *d_i_vectors, *d_norm;
+    int size_double = SIZE * DIMENSIONS * sizeof(double);
+
+    y = (double *)malloc(size_double);
+    x = (double *)malloc(size_double);
+    z = (double *)malloc(size_double);
+
+    kernel = (double *)malloc(SIZE * SIZE * sizeof(double));
+    i_vectors = (double *)malloc(SIZE * DIMENSIONS * SIZE * sizeof(double));
+    norm = (double *)malloc(SIZE * sizeof(double));
+
+    file = fopen("data_cuda_seeds.bin", "rb");
+    if (file == NULL)
+    {
+        printf("Could not open file\n");
+        exit(1);
+    }
+
+    if(fread(y, sizeof(double), SIZE * DIMENSIONS, file) != SIZE*DIMENSIONS){
+        printf("Error at reading file\n");
+        exit(1);
+    }
+
+    fclose(file);
+
+    for(int i = 0; i < SIZE * DIMENSIONS; i++)
+    {
+        x[i] = y[i];
+    }
+
+    h = 1.4565;
+
+    cudaMalloc((void **)&d_y, size_double);
+    cudaMalloc((void **)&d_x, size_double);
+    cudaMalloc((void **)&d_h, sizeof(double));
+    cudaMalloc((void **)&d_kernel, SIZE * SIZE * sizeof(double));
+    cudaMalloc((void **)&d_i_vectors, SIZE * DIMENSIONS * SIZE * sizeof(double));
+    cudaMalloc((void **)&d_norm, SIZE * sizeof(double));
+
+    cudaMemcpy(d_y, y, size_double, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_x, x, size_double, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_h, &h, sizeof(double), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_kernel, kernel, SIZE * SIZE * sizeof(double), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_i_vectors, i_vectors, SIZE * DIMENSIONS * SIZE * sizeof(double), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_norm, norm, SIZE * sizeof(double), cudaMemcpyHostToDevice);
+
+    //Time variables.
+    struct timeval startwtime, endwtime;
+
+    gettimeofday (&startwtime, NULL);
+
+    mean_shift<<<SIZE,SIZE>>>(d_y, d_x, d_h, d_kernel, d_i_vectors, d_norm);
+
+    cudaDeviceSynchronize();
+
+    gettimeofday (&endwtime, NULL);
+
+    cudaMemcpy(z, d_y, size_double, cudaMemcpyDeviceToHost);
+
+    double exec_time = (double)((endwtime.tv_usec - startwtime.tv_usec)/1.0e6
+    + endwtime.tv_sec - startwtime.tv_sec);
+    printf("Runtime: %.14f \n", exec_time);
+
+    file = fopen("results_global_seeds.bin", "wb");
+    if (file == NULL)
+    {
+        printf("Could not open file\n");
+        exit(1);
+    }
+
+    fwrite(z, sizeof(double), SIZE * DIMENSIONS, file);
+
+    fclose(file);
+
+    cudaFree(d_y);
+    cudaFree(d_x);
+    cudaFree(d_h);
+    cudaFree(d_kernel);
+    cudaFree(d_i_vectors);
+    cudaFree(d_norm);
+
+    free(y);
+    free(x);
+	free(z);
+    free(kernel);
+    free(i_vectors);
+    free(norm);
+
+    return 0;
+}
---
+++ mean_shift_shared_seeds.cu
@@ -0,0 +1,192 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <sys/time.h>
+#include <cuda.h>
+
+#define SIZE 210
+#define DIMENSIONS 7
+
+__global__ void mean_shift(double *y, double *x, double *h)
+{
+  //Shared variables.
+  __shared__ double kernel[SIZE];
+  __shared__ double i_vectors[SIZE*DIMENSIONS];
+  __shared__ double norm;
+
+  //Thread scoped variables.
+  int basic_index = blockIdx.x * (DIMENSIONS);
+  int index = threadIdx.x * (DIMENSIONS);
+  double e = 0.0001;
+  double factor;
+
+  //Initializing the norm.
+  norm = 1.0;
+
+  //Main loop.
+  while (norm > e) {
+
+    factor = 0.0;
+    //Calculating factor.
+    for (int i = 0; i < (DIMENSIONS); i++)
+    {
+        factor += (y[basic_index + i] - x[index + i])*(y[basic_index + i] - x[index + i]);
+    }
+
+    if (sqrt(factor) > *h*(*h))
+    {
+      kernel[threadIdx.x] = 0.0;
+      for (int i = 0; i < (DIMENSIONS); i++)
+      {
+        i_vectors[index+i] = 0.0;
+      }
+    }
+    else
+    {
+      factor = exp(-(factor)/(2* (*h) * (*h)));
+
+      kernel[threadIdx.x] = factor;
+
+      //Storing the 'y' vectors before normalization.
+      for (int i = 0; i < (DIMENSIONS); i++)
+      {
+        i_vectors[index+i] = factor * x[index + i];
+      }
+    }
+
+    __syncthreads();
+
+    //Only the first thread of each block will calculate
+    //the final vectors.
+    if (threadIdx.x == 0)
+    {
+      double y_new[DIMENSIONS];
+      for (int i = 0; i < (DIMENSIONS); i++)
+      {
+	       y_new[i] = 0.0;
+      }
+
+      double sum = 0.0;
+
+      for (int i = 0; i < (SIZE) * (DIMENSIONS); i += (DIMENSIONS))
+      {
+      	for (int j = 0; j < (DIMENSIONS); j++)
+      	{
+      	  y_new[j] += i_vectors[i + j];
+      	}
+      }
+
+      //Calculating the sum of each row of the kernel_matrix.
+      for (int i = 0; i < (SIZE); i++)
+      {
+        sum += kernel[i];
+      }
+
+      //Normalizing and updating the 'y' vector.
+      for (int i = 0; i < (DIMENSIONS); i++)
+      {
+        y_new[i] = y_new[i] / sum;
+      }
+
+      //Re-initializing norm.
+      norm = 0.0;
+
+      //Calculating the norm of the vector
+      //and updating the 'y' vector
+      for (int i = 0; i < (DIMENSIONS); i++)
+      {
+
+        norm += (y_new[i]-y[basic_index + i])*(y_new[i]-y[basic_index + i]);
+        y[basic_index + i] = y_new[i];
+
+      }
+
+      norm = sqrt(norm);
+
+    }
+
+    __syncthreads();
+
+  }
+
+}
+
+int main(int argc, char **argv)
+{
+    FILE *file;
+    double *y, *x, *z, h;
+    double *d_y, *d_x, *d_h;
+    int size_double = SIZE * DIMENSIONS * sizeof(double);
+
+    y = (double *)malloc(size_double);
+    x = (double *)malloc(size_double);
+    z = (double *)malloc(size_double);
+
+    file = fopen("data_cuda_seeds.bin", "rb");
+    if (file == NULL)
+    {
+        printf("Could not open file\n");
+        exit(1);
+    }
+
+    if (fread(y, sizeof(double), SIZE * DIMENSIONS, file) != SIZE * DIMENSIONS)
+    {
+        printf("Error at reading file\n");
+        exit(1);
+    }
+
+    fclose(file);
+
+    for(int i = 0; i < SIZE * DIMENSIONS; i++)
+    {
+        x[i] = y[i];
+    }
+
+    h = 1.4565;
+
+    cudaMalloc((void **)&d_y, size_double);
+    cudaMalloc((void **)&d_x, size_double);
+    cudaMalloc((void **)&d_h, sizeof(double));
+
+    cudaMemcpy(d_y, y, size_double, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_x, x, size_double, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_h, &h, sizeof(double), cudaMemcpyHostToDevice);
+
+    //Time variables.
+    struct timeval startwtime, endwtime;
+
+    gettimeofday (&startwtime, NULL);
+
+    mean_shift<<<SIZE,SIZE>>>(d_y, d_x, d_h);
+
+    cudaDeviceSynchronize();
+
+    gettimeofday (&endwtime, NULL);
+
+    cudaMemcpy(z, d_y, size_double, cudaMemcpyDeviceToHost);
+
+    double exec_time = (double)((endwtime.tv_usec - startwtime.tv_usec)/1.0e6
+    + endwtime.tv_sec - startwtime.tv_sec);
+    printf("Runtime: %.14f \n", exec_time);
+
+    file = fopen("results_shared_seeds.bin", "wb");
+    if (file == NULL)
+    {
+        printf("Could not open file\n");
+        exit(1);
+    }
+
+    fwrite(z, sizeof(double), SIZE * DIMENSIONS, file);
+
+    fclose(file);
+
+    cudaFree(d_y);
+    cudaFree(d_x);
+    cudaFree(d_h);
+
+    free(y);
+    free(x);
+	free(z);
+
+    return 0;
+}
